16c992bdad081b6b9819acf9836e845db28b4ea3
kotathefriends <5070kouta@gmail.com>
2023-12-02 12:49:31 +0900

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by KOTA TAKAHASHI on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
